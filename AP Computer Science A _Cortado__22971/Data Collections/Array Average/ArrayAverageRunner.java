5d573b23e686df7c39b29ef6fd66ec5789099c1e
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-06-05 16:29:38 +0000

    Review: ArrayAverageRunner.java - 2026-06-05 [2/9]

--- AP Computer Science A _Cortado__22971/Data Collections/Array Average/ArrayAverageRunner.java
+++ AP Computer Science A _Cortado__22971/Data Collections/Array Average/ArrayAverageRunner.java
@@ -5,4 +5,4 @@ public class ArrayAverageRunner
         int[] numArray = {12, 17, 65, 7, 30, 88};
         System.out.println("The average of the array is " + ArrayAverage.getAverage(numArray));
     }
-}
+}// reviewed: 2026-06-05
