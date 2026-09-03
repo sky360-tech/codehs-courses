fc9a82a336e0ae82dcef6da323181b960ded1561
ASM Tahmir Hasan <tahmirhasanraid1@gmail.com>
2026-09-03 17:31:59 +0000

    Review: Obi-WanSays.js - 2026-09-03 [5/9]

--- AP Computer Science Principles in JavaScript_6166/Programming with JavaScript/Obi-WanSays.js
+++ AP Computer Science Principles in JavaScript_6166/Programming with JavaScript/Obi-WanSays.js
@@ -3,4 +3,4 @@ var name = readLine("YourName? ");
 var age = readInt("How many driods do you want to met?");
 var wookies = readInt("How many wookies? ");
 println("Hi " + name + " you want to meet " + age + " droids and " + wookies + " Wookie.");
-}
+}// reviewed: 2026-09-03
